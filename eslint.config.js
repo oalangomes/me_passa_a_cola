c6cb14587e07ae0b7eda23fd49eb4ac2fd8fc20d
Alan Gomes <52175262+oalangomes@users.noreply.github.com>
2025-07-08 21:28:48 -0300

    feat: adiciona lint e atualiza workflow

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 2021,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: false,
+    },
+    rules: {
+    }
+  }
+];
